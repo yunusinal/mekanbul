5931a2528c5378843736a6a2877845afeef552a4
yunus <yunusinal00@gmail.com>
2024-12-28 11:57:08 +0300

    backend and fronted link update

--- backend/app_api/models/db.js
+++ backend/app_api/models/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var dbURI = "mongodb://localhost:27017/mekanbul";
+var dbURI = "mongodb://localhmongodb+srv://yunus:1234@cluster0.9k0pz.mongodb.net/?retryWrites=true&w=majority&appName=cluster0ost:27017/mekanbul";
 mongoose.connect(dbURI);
 mongoose.connection.on("connected", function () {
     console.log(dbURI + " adresine bağlandı");
--- frontend/src/services/http-common.jsx
+++ frontend/src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://mekanbul-4ebvhkc05-yunusinals-projects.vercel.app/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
